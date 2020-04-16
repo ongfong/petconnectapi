f654affe5c9c8dc09339f170e8d41caab4e89045
hipeople <dongfong9@gmail.com>
2020-04-16 22:08:55 +0700

    test

--- routes/category.js
+++ routes/category.js
@@ -7,7 +7,7 @@ const { runValidation } = require('../validators');
 const { categoryCreateValidator } = require('../validators/category');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
-router.post('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.post('/category', categoryCreateValidator, create);
 router.get('/categories', list);
 router.get('/category/:slug', read);
 router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
