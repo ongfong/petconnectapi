3f9e57fde4f564be0127a844443cf6843c403b10
hipeople <dongfong9@gmail.com>
2020-04-30 17:08:24 +0700

    update

--- validators/tag.js
+++ validators/tag.js
@@ -15,19 +15,7 @@ exports.requireTagValidator = [
     check('district')
         .not()
         .isEmpty()
-        .withMessage('district is required'),
-    check('zone')
-        .not()
-        .isEmpty()
-        .withMessage('zone is required'),
-    check('province')
-        .not()
-        .isEmpty()
-        .withMessage('zone is province'),
-    check('postalCode')
-        .not()
-        .isEmpty()
-        .withMessage('postal Code is required'),
+        .withMessage('district is required')
 ];
 
 exports.registerTagValidator = [
